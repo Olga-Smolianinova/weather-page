c73dccaa8db7a4fdfface728f911d9462075df75
Olga-Smolianinova <olga.smol2015@gmail.com>
2021-08-17 00:02:37 +0300

    add homepage

--- src/api/weather-api.js
+++ src/api/weather-api.js
@@ -1,9 +1,10 @@
-const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+// const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
 
 const fetchWeather = async ({ searchQuery = '' }) => {
   try {
+    const api_key = '22301d37d5f356a26164cebb551f003e';
     const response = await fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${searchQuery}&units=metric&appid=${API_KEY}`,
+      `http://api.openweathermap.org/data/2.5/weather?q=${searchQuery}&units=metric&appid=${api_key}`,
     );
     const weather = response.json();
     // console.log(weather);
@@ -15,8 +16,9 @@ const fetchWeather = async ({ searchQuery = '' }) => {
 
 const fetchWeekWeather = async ({ lon = '', lat = '' }) => {
   try {
+    const api_key = '22301d37d5f356a26164cebb551f003e';
     const response = await fetch(
-      `http:///api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely&units=metric&appid=${API_KEY}`,
+      `http:///api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely&units=metric&appid=${api_key}`,
     );
     const weekWeather = response.json();
     return weekWeather;
